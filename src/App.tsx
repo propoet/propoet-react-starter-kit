94271e13a0c12f001ccacb2653c0110092acdd83
zhangcm <propet@163.com>
2025-05-30 11:51:24 +0800

    feat: add routing and request functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
+import { Routes,Route,Link} from 'react-router-dom'
+import Home from './views/home'
+import About from './views/about'
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+   <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+   </div>
   );
 }
 
